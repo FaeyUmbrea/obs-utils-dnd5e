97eea9f7d72ba13e9b30a8c550660f84b38f4a17
Faey Umbrea <1144986+FaeyUmbrea@users.noreply.github.com>
2023-07-31 04:36:20 +0200

    Small Fix

--- src/polymorph.js
+++ src/polymorph.js
@@ -1,26 +1,26 @@
-Hooks.on('dnd5e.revertOriginalForm',(actor)=>{
-    const currentId = actor.id;
-    const originalActor = actor.flags.dnd5e.originalActor;
-    replaceActor(currentId,originalActor);
-})
+Hooks.on('dnd5e.revertOriginalForm', (actor) => {
+  const currentId = actor.id;
+  const originalActor = actor.flags.dnd5e.originalActor;
+  replaceActor(currentId, originalActor);
+});
 
-Hooks.on('dnd5e.transformActor',(from,to)=>{
-    const currentId = from.id;
-    const hookId = Hooks.on("createActor",(actor)=>{
-        const originalActor = actor.flags?.dnd5e?.originalActor;
-        if(originalActor === currentId){
-            const newId = actor.id;
-            replaceActor(currentId,newId)
-            Hooks.off("createActor",hookId);
-        }
-    })
-})
-
-function replaceActor(currentId, newId){
-    const selectedActors = game.modules.get('obs-utils').api.getSelectedActors()
-    const index = selectedActors.findIndex((element)=>element === currentId)
-    if(index > -1){
-        selectedActors[index] = newId
-        game.modules.get('obs-utils').api.setSelectedActors(selectedActors)
+Hooks.on('dnd5e.transformActor', (from) => {
+  const currentId = from.id;
+  const hookId = Hooks.on('createActor', (actor) => {
+    const originalActor = actor.flags?.dnd5e?.originalActor;
+    if (originalActor === currentId) {
+      const newId = actor.id;
+      replaceActor(currentId, newId);
+      Hooks.off('createActor', hookId);
     }
+  });
+});
+
+function replaceActor(currentId, newId) {
+  const selectedActors = game.modules.get('obs-utils').api.getSelectedActors();
+  const index = selectedActors.findIndex((element) => element === currentId);
+  if (index > -1) {
+    selectedActors[index] = newId;
+    game.modules.get('obs-utils').api.setSelectedActors(selectedActors);
+  }
 }
